d4e4acac690fab90b96fa5393f3ef06c2a176108
Lielco <mslielcohen@gmail.com>
2023-06-20 15:10:55 -0400

    Connect backend to front

--- frontend/src/app/listmessages/listmessages.component.ts
+++ frontend/src/app/listmessages/listmessages.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-listmessages',
   templateUrl: './listmessages.component.html',
-  styleUrls: ['./listmessages.component.css']
+  styleUrls: ['./listmessages.component.scss']
 })
 export class ListmessagesComponent {
+  messages:any;
 
+  constructor(private messageService: MessageService) {
+    this.messages = messageService.listMessages().subscribe(
+      message => {
+        this.messages = message
+        console.log(this.messages);
+      }
+    )
+  }
+
+  MessageList() {
+
+  }
 }
--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  url:string = "http://127.0.0.1:3000";
+  constructor(private http: HttpClient) { }
+
+  listMessages() {
+    return this.http.get(this.url+'/api/v1/messages');
+  }
 }
